5ea18a6ea5980545f4266a901011897393b17cf6
Romulo Beninca <rbeninca@gmail.com>
2026-04-28 21:27:14 -0300

    Criado tela com resultado do calculo

--- app/src/main/java/com/example/app/MainActivity2.java
+++ app/src/main/java/com/example/app/MainActivity2.java
@@ -1,6 +1,7 @@
 package com.example.app;
 
 import android.os.Bundle;
+import android.widget.TextView; // Importante para exibir o resultado
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +16,24 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main2);
+
+        Bundle extras = getIntent().getExtras();
+
+        if (extras != null) {
+            String sPeso = extras.getString("PESO");
+            String sAltura = extras.getString("ALTURA");
+
+            if (sPeso != null && sAltura != null) {
+                double valorPeso = Double.parseDouble(sPeso);
+                double valorAltura = Double.parseDouble(sAltura);
+
+                double imc = valorPeso / (valorAltura * valorAltura);
+
+                TextView resultado = findViewById(R.id.textViewResultado);
+                resultado.setText(String.format("Seu IMC é: %.2f", imc));
+            }
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
